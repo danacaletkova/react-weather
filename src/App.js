989b6597cbe3c19bd627c866daaf89172658d8a6
danacaletkova <164472411+danacaletkova@users.noreply.github.com>
2024-05-13 10:55:35 +0800

    Add footer with github and netlify links

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import './App.css';
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      Weather app
+      <h1>Weather app</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/danacaletkova" target="_blank">
+          Dana Caletkova
+        </a>{" "}
+        and is open-sourced on{" "}
+        <a
+          href="https://github.com/danacaletkova/react-weather"
+          target="_blank"
+        >
+          GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a href="https://meteo-react.netlify.app/" target="_blank">
+          Netlify
+        </a>
+      </footer>
     </div>
   );
 }
