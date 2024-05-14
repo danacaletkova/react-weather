f2a5874b667c49443edea8303d75f1122d3dd511
danacaletkova <164472411+danacaletkova@users.noreply.github.com>
2024-05-14 13:41:04 +0800

    Allow temperature unit conversion

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,26 +1,22 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
       <div className="row py-4 px-1">
-        <div className="col-6 d-flex align-items-end">
+        <div className="col-7 d-flex align-items-end">
           <WeatherIcon code={props.data.icon} />
-          <p>
-            <span className="temperature">
-              {Math.round(props.data.temperature)}
-            </span>
-            <span className="unit">°C</span>
-          </p>
+          <WeatherTemperature celsius={props.data.temperature} />
           <ul>
             <li>Feels like: {Math.round(props.data.feelsLike)} °C</li>
             <li>Humidity: {props.data.humidity} %</li>
             <li>Wind: {props.data.wind} km/h</li>
           </ul>
         </div>
-        <div className="col-6 text-end">
+        <div className="col-5 text-end">
           <h1>{props.data.city}</h1>
           <ul>
             <li>
