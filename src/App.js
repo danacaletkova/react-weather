89ef5c66dde2f37f19ae367b96d95c1a8c1f8942
danacaletkova <164472411+danacaletkova@users.noreply.github.com>
2024-05-16 11:52:09 +0800

    Shorten footer

--- src/App.js
+++ src/App.js
@@ -7,30 +7,20 @@ export default function App() {
     <div className="App">
       <div className="container">
         <Weather defaultCity="Perth" />
-        <footer className="px-2">
-          This project was coded by{" "}
+        <footer className="p-2">
+          <a
+            href="https://github.com/danacaletkova/react-weather"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source </a>
+          code by{" "}
           <a
             href="https://github.com/danacaletkova"
             target="_blank"
             rel="noreferrer"
           >
             Dana Caletkova
-          </a>{" "}
-          and is open-sourced on{" "}
-          <a
-            href="https://github.com/danacaletkova/react-weather"
-            target="_blank"
-            rel="noreferrer"
-          >
-            GitHub
-          </a>{" "}
-          and hosted on{" "}
-          <a
-            href="https://meteo-react.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Netlify
           </a>
         </footer>
       </div>
